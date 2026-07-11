c6c63c390770210d14c5da6ed9a9325c8e5b3429
azk <azkmood@gmail.com>
2026-07-12 00:17:36 +0700

    feat: menambahkan status kendaraan utama

---
+++ backend/database/migrations/2026_07_03_122724_add_is_primary_to_vehicles_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('vehicles', function (Blueprint $table) {
+
+            $table->boolean('is_primary')
+                  ->default(false)
+                  ->after('color');
+
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('vehicles', function (Blueprint $table) {
+
+            $table->dropColumn('is_primary');
+
+        });
+    }
+};
